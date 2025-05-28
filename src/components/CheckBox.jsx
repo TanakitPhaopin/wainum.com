b2b7d151fd98bf067ae391eacf288840e8135a34
Tanakit Phaopin <ocheer12@hotmail.com>
2025-05-28 23:45:49 +1000

    Fix typo

--- src/components/CheckBox.jsx
+++ src/components/CheckBox.jsx
@@ -1,7 +1,7 @@
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 
-export default function MyCheckbox({check, label, onChange}) {
+export default function MyCheckBox({check, label, onChange}) {
   return (
       <FormControlLabel control={<Checkbox checked={check} />} label={label} onChange={onChange}/>
   );
