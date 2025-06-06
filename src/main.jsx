6911aeec3e70f124e824f987690b934854e54a1c
Tanakit Phaopin <ocheer12@hotmail.com>
2025-06-06 12:11:20 +1000

    Add react-sap to help with seo

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,43 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContext.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { AuthProvider } from './contexts/AuthContext.jsx'
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <AuthProvider>
+//       <LocalizationProvider dateAdapter={AdapterMoment}>
+//         <App />
+//       </LocalizationProvider>
+//     </AuthProvider>
+//   </StrictMode>,
+// )
+
+import React from 'react';
+import { hydrate, render } from 'react-dom'; // instead of createRoot
+import './index.css';
+import App from './App.jsx';
+import { AuthProvider } from './contexts/AuthContext.jsx';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const Root = () => (
+  <React.StrictMode>
     <AuthProvider>
       <LocalizationProvider dateAdapter={AdapterMoment}>
         <App />
       </LocalizationProvider>
     </AuthProvider>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<Root />, rootElement);
+} else {
+  render(<Root />, rootElement);
+}
